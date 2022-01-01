15793187b233ba405ffdbdd1ac7cfeaa300d3196
Guru-Nadhan <guru.sivaram1@gmail.com>
2022-01-01 21:51:09 +0530

    updated

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -4,7 +4,7 @@ function Welcome() {
     return (
         <div className={Style.container}>
             <h1 className={Style.heading}>Welcome to the recipe app</h1>
-            <p className={Style.para}>This is a React app for searching recipes. This app fetches the <a href="https://developer.edamam.com/edamam-docs-recipe-api" target='_blank' className={Style.anchor}>'Recipe Search' API</a> from Edamam website which contains over 2.3 million recipes. You can also refer the calories of a particular recipe. Just search the dish name and the recipes if the related dishes will appear. If no recipes are found, then this welcome message will be displayed. Have a great cooking!
+            <p className={Style.para}>This is a React app for searching recipes. This app fetches the <a href="https://developer.edamam.com/edamam-docs-recipe-api" target='_blank' className={Style.anchor}>'Recipe Search' API</a> from Edamam website which contains over 2.3 million recipes. You can also refer the calories of a particular recipe. Just search the dish name and the recipes of the related dishes will appear. If no recipes are found, then this welcome message will be displayed. Have a great cooking!
             </p>
             <div className={Style.credits}>Made with love by <a href="https://guru-nadhan.github.io/Guru-Portfolio/" target='_blank' className={Style.anchor}>Guru Nadhan</a></div>
         </div>
